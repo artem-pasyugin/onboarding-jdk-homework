57180284e1b13588a7185c2578d41e45cc85af84
Artem Pasyugin <APasyugin@alfabank.ru>
2022-03-15 22:11:32 +0300

    Algorithms template

---
+++ src/test/java/algorithms/HomeWork1Test.java
@@ -0,0 +1,11 @@
+package algorithms;
+
+import org.junit.jupiter.api.Test;
+
+public class HomeWork1Test {
+
+    @Test
+    public void testCase1() {
+
+    }
+}
---
+++ src/test/java/algorithms/HomeWork2Test.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+import org.junit.jupiter.api.Test;
+
+public class HomeWork2Test {
+
+    @Test
+    public void testCase1() {
+
+    }
+
+}
