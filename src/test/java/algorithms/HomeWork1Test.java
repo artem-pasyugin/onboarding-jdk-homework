0c2ff3d679142d43590f50b5b2d28d21d1c4f769
aizh-pro <68395988+aizh-pro@users.noreply.github.com>
2022-03-19 17:58:42 +0600

    HomeWorks algorithms are ready (#8)
    Co-authored-by: Artem Pasyugin

--- src/test/java/algorithms/HomeWork1Test.java
+++ src/test/java/algorithms/HomeWork1Test.java
@@ -2,10 +2,39 @@ package algorithms;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class HomeWork1Test {
+    private final HomeWork1 homework = new HomeWork1();
 
     @Test
     public void testCase1() {
+        int[] arr = {1, 1, 2, 3, 4};
+        assertFalse(homework.checkIsUnique(arr));
+    }
 
+    @Test
+    public void testCase2() {
+        int[] arr = {1, 0, 2, 3, 3};
+        assertFalse(homework.checkIsUnique(arr));
+    }
+
+    @Test
+    public void testCase3() {
+        int[] arr = {1, 2, 3, 4, 5};
+        assertTrue(homework.checkIsUnique(arr));
+    }
+
+    @Test
+    public void testCase4() {
+        int[] arr = {1, 4, 2, 2, 3, 6};
+        assertFalse(homework.checkIsUnique(arr));
+    }
+
+    @Test
+    public void testCase5() {
+        int[] arr = {5, 5};
+        assertFalse(homework.checkIsUnique(arr));
     }
 }
--- src/test/java/algorithms/HomeWork2Test.java
+++ src/test/java/algorithms/HomeWork2Test.java
@@ -1,12 +1,40 @@
 package algorithms;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HomeWork2Test {
+    private final HomeWork2 homework = new HomeWork2();
 
     @Test
     public void testCase1() {
-
+        int[] data = {0, 2, -8, 5, 1};
+        int[] expectedResult = {-8, 0, 1, 2, 5};
+        int[] actualResult = homework.sortByAscendingOrder(data);
+        Assertions.assertArrayEquals(expectedResult, actualResult);
     }
 
+    @Test
+    public void testCase2() {
+        int[] data = {0, 2, 2, 5, 0};
+        int[] expectedResult = {0, 0, 2, 2, 5};
+        int[] actualResult = homework.sortByAscendingOrder(data);
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testCase3() {
+        int[] data = {};
+        int[] expectedResult = {};
+        int[] actualResult = homework.sortByAscendingOrder(data);
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testCase4() {
+        int[] data = {0};
+        int[] expectedResult = {0};
+        int[] actualResult = homework.sortByAscendingOrder(data);
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
 }
