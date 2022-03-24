f38e5a273081dcfa3bac3027dc22e7eab3eec60f
Dmitriy-kuprienko <kuprienko.dv@mail.ru>
2022-03-24 14:05:18 +0300

    home work1

--- src/main/java/algorithms/HomeWork1.java
+++ src/main/java/algorithms/HomeWork1.java
@@ -1,32 +1,15 @@
 package algorithms;
 
-/**
- * Домашнее задание №1.
- * Дано: Целочисленный массив
- * Ожидание: Проверить на уникальность всех элементов в массиве,
- * не создавая при этом новый массив
- */
-public class HomeWork1 {
 
-    /**
-     * На входе целочисленный массив
-     * Необходимо проверить все ли элементы массива различны
-     *
-     * @param numbers - массив. Пример: [1,2,3]
-     * @return - true массив уникален, а false не уникален
-     */
     public boolean checkIsUnique(int[] numbers) {
-        oolean result = true;
+        boolean result = true;
         for (int i = 0; i < numbers.length ; i++) {
-//            System.out.println(numbers[i]);
-//            System.out.println("__________");
+
 
             for (int j = i + 1; j < numbers.length ; j++) {
                 if (numbers[i] == numbers[j]){
                     result = false;
                 }
-//                System.out.println(j);
-//                System.out.println(result);
 
             }
         }
--- src/main/java/algorithms/HomeWork2.java
+++ src/main/java/algorithms/HomeWork2.java
@@ -1,18 +1,8 @@
 package algorithms;
 
-/**
- * Домашнее задание №2.
- * Дано: Целочисленный массив
- * Ожидание: упорядочить список по возрастанию
- */
+
 public class HomeWork2 {
-    /**
-     * На входе целочисленный массив
-     * Необходимо отсортировать массив по возрастанию
-     *
-     * @param numbers - массив. Пример: [1,2]
-     * @return - отсортированный массив
-     */
+
     public int[] sortByAscendingOrder(int numbers[]) {
         boolean result = false;
         int k = 1000;
@@ -35,7 +25,7 @@ public class HomeWork2 {
             System.out.print(number + ", ");
 
         }
-        System.out.print("--------");
+
 
 
         return numbers;
