280538bb59cebafd0c819323c5da6b3157f8ac08
andrei_doronin_1 <anddoron@yandex.ru>
2022-08-31 20:29:59 +0300

    Homework_algorithms

--- src/main/java/example/HomeWork1.java
+++ src/main/java/example/HomeWork1.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.util.Arrays;
+
 /**
  * Домашнее задание №1.
  * Дано: целочисленный массив numbers размером n.
@@ -14,8 +16,24 @@ class HomeWork1 {
      * @return возвращаемый целочисленный массив, пример [1, 2, 1, 1, 2, 1].
      */
     public int[] run(int[] numbers) {
+        int g = numbers.length;
+        int[] twonumbers = new int[g * 2];
+        int l = 0;
+        int b = g;
+        while (l < g) {
+            twonumbers[l] = numbers[l];
+            l++;
+        }
+        for (int i = g; i < twonumbers.length; i++) {
+            twonumbers[i] = twonumbers[g - (b--)];
+
+        }
+
+
+        String twonumbersString = Arrays.toString(twonumbers);
+        System.out.println(twonumbersString);
+
         // TODO реализовать тут
-        return null;
-    }
+        return twonumbers;}
 
 }
--- src/main/java/example/HomeWork2.java
+++ src/main/java/example/HomeWork2.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.util.Arrays;
+
 /**
  * Домашнее задание №1.
  * Дано: целочисленный массив numbers размером n.
@@ -14,8 +16,34 @@ class HomeWork2 {
      * @return - целочисленный массив, пример результата [6, 2, 5, 1].
      */
     public int[] run(int[] numbers) {
+        int p = numbers.length;
+        int k = 0;
+        int f = 0;
+        int t;
+        int l = 0;
+        int[] ordernumbers = new int[p];
+        for (int i = 0; i < p; i++) {
+            if (numbers[k] != 1 && numbers[k] % 2 == 0) {
+                ordernumbers[f] = numbers[k];
+                f++;
+            }
+            k++;
+        }
+
+        t = f;
+
+        for (int h = 0; h < p; h++) {
+            if (numbers[l] == 1 || numbers[l] % 2 == 1) {
+                ordernumbers[t] = numbers[l];
+                t++;
+            }
+            l++;
+        }
+        String ordernumbersString = Arrays.toString(ordernumbers);
+        System.out.println(ordernumbersString);
         // TODO реализовать тут
-        return null;
+        return ordernumbers;
+
     }
 
 }
