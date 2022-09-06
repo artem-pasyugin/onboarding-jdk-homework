641a6196c95d6af01292e6f91d06e89a809d409b
armen-babayan <arm_9876@mail.ru>
2022-09-06 20:11:47 +0300

    исправил дз

--- src/main/java/example/HomeWork2.java
+++ src/main/java/example/HomeWork2.java
@@ -13,18 +13,18 @@ class HomeWork2 {
      * @param numbers - целочисленный массив, пример [5, 6, 1, 2].
      * @return - целочисленный массив, пример результата [6, 2, 5, 1].
      */
-    public int[] run(int[] numbers) {
-        int[] newMassive = new int[numbers.length];
-        int j=0;
-        for (int i=0; i<numbers.length; i++){
-            if (numbers[i]%2==0) {
-                newMassive[j]=numbers[i];
+    public int[] run ( int[] numbers ) {
+        int[] newMassive = new int[ numbers.length ];
+        int j = 0;
+        for ( int i = 0 ; i < numbers.length ; i++ ) {
+            if ( numbers[ i ] % 2 == 0 ) {
+                newMassive[ j ] = numbers[ i ];
                 j++;
             }
         }
-        for (int i=0; i<numbers.length; i++){
-            if (numbers[i]%2!=0) {
-                newMassive[j]=numbers[i];
+        for ( int i = 0 ; i < numbers.length ; i++ ) {
+            if ( numbers[ i ] % 2 != 0 ) {
+                newMassive[ j ] = numbers[ i ];
                 j++;
             }
         }
